c09cfc0b08d80b30c18d7e82d761c683c3442106
Ulyana Ermoshina <uermoshina@inbox.ru>
2020-07-17 20:43:51 +0300

    In commit

---
+++ src/test/java/ru/netology/repository/ProductRepositoryTest.java
@@ -0,0 +1,34 @@
+package ru.netology.repository;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.netology.domain.Book;
+import ru.netology.domain.Product;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductRepositoryTest {
+    private ProductRepository productRepository = new ProductRepository();
+
+    Book book1 = new Book(1,"Tom",100,"Lev",20,1950);
+    Book book2 = new Book(3,"Tom",100,"Sid",20,1950);
+
+    @BeforeEach
+     public void setUp() {
+        productRepository.save(book1);
+        productRepository.save(book2);
+    }
+
+    @Test
+    public void shouldDeleteNotExistingElement() {
+        assertThrows(NotFoundException.class, () -> productRepository.removeById(2));
+    }
+
+    @Test
+    public void shouldDeleteExistingElement() {
+        productRepository.removeById(1);
+        Product[] actual = productRepository.findAll();
+        Product[] expected = new Product[]{book2};
+        assertArrayEquals(actual,expected);
+    }
+}
